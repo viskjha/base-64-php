877a7fc068ea5d6fac1a70cd5bd6de02e8ab0f9a
viskjha <vishaljha511@gmail.com>
2019-07-28 00:36:37 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+   $path = 'vishalpic.jpg';
+$type = pathinfo($path, PATHINFO_EXTENSION);
+$data = file_get_contents($path);
+$base64 = 'data:image/' . $type . ';base64,' . base64_encode($data);
+
+echo "<img src='$base64'/>";
+
+?>
+</body>
+</html>
